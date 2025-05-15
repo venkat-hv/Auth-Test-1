f03068c634213c02a0f929e58e795f6172a9e4ac
Sai Venkat <kondeti.venkat@herovired.com>
2025-05-15 15:08:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+require("dotenv").config({ path: ".env.production" });
+
+const app = express();
+const PORT = 4000;
+
+const AUTH_SERVER = process.env.AUTH_SERVER;
+const HOST = process.env.HOST;
+
+// Middleware (SESSION, COOKIES)
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+
+async function auth1(req, res, next) {
+  try {
+    const token = req.cookies["auth_token"];
+
+    let user;
+    if (!token) {
+      const redirect = `${AUTH_SERVER}/auth/google?redirect=${HOST}/dashboard`;
+      return res.redirect(redirect);
+    } else {
+      try {
+        const response = await axios.get(`${AUTH_SERVER}/me`);
+        user = response.data.user;
+      } catch (error) {
+        console.log(
+          `ERROR in authentication with response (${error.response.status}): `,
+          error.response.data
+        );
+        return res.status(400).json({ message: "Unauthorized" });
+      }
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("ERROR in auth1: ", error);
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function auth2(req, res, next) {
+  try {
+    const token = req.cookies["auth_token"];
+
+    let user;
+    if (!token) {
+      const redirect = `${AUTH_SERVER}/auth/google?redirect=${HOST}/dashboard`;
+      return res.redirect(redirect);
+    } else {
+      user = jwt.verify(token, JWT_SECRET); // Needs shared secret
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("ERROR in auth2: ", error);
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send(
+    '<h1>Welcome to the Client App</h1><a href="/dashboard">Go to Dashboard</a>'
+  );
+});
+
+app.get("/dashboard", auth1, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(
+      `<h2>Welcome, ${user.name}</h2><p>Email: ${user.email}</p><br/><p>Email: ${user.role}</p><a href="/logout">Logout</a>`
+    );
+  } catch (err) {
+    console.error(err);
+    res.redirect("/");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("auth_token");
+
+  return res.redirect(`${AUTH_SERVER}/logout?redirect=${HOST}/`);
+});
+
+app.listen(PORT, () => console.log("Client running on port: ", PORT));
